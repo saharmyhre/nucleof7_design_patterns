c430608b012a9615c76090735a174b1ce8024ac3
Sahar Myhre <saharmyhre@gmail.com>
2025-10-07 15:50:21 -0700

    Enable FPU

---
+++ 1_systick_fpu/Src/main.c
@@ -0,0 +1,31 @@
+#include "stm32f7xx.h"
+#include "fpu.h"
+
+#define LED_PORT GPIOB
+#define LED_PORT_CLOCK (1U << 1)
+
+#define LED_RED_PIN (1U<<14)
+#define LED_GREEN_PIN (1U<<0)
+#define LED_BLUE_PIN (1U<<7)
+
+#define LED_RED_MODE_BIT (1U<<28)
+#define LED_GREEN_MODE_BIT (1U<<0)
+#define LED_BLUE_MODE_BIT (1U<<14)
+
+int main(void)
+{
+
+	fpu_enable();
+	RCC->AHB1ENR |= LED_PORT_CLOCK;
+
+    LED_PORT->MODER |= LED_RED_MODE_BIT;
+    LED_PORT->MODER |= LED_GREEN_MODE_BIT;
+
+	while (1)
+    {
+		LED_PORT->ODR ^= LED_RED_PIN;
+		for (int i = 0; i < 100000; ++i)
+		{}
+		LED_PORT->ODR ^= LED_GREEN_PIN;
+    }
+}
