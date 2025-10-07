58da152b0cf2fb8562a8c02a8394ea58ce2cb937
Sahar Myhre <saharmyhre@gmail.com>
2025-10-07 15:11:43 -0700

    Add structure & blinking LED

---
+++ 0_test/Debug/Src/main.d
@@ -0,0 +1,17 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/sahar/repo/nucleof7_design_patterns/0_test/chip_headers/CMSIS/Device/ST/STM32F7xx/Include/stm32f7xx.h \
+ C:/Users/sahar/repo/nucleof7_design_patterns/0_test/chip_headers/CMSIS/Device/ST/STM32F7xx/Include/stm32f765xx.h \
+ C:/Users/sahar/repo/nucleof7_design_patterns/0_test/chip_headers/CMSIS/Include/core_cm7.h \
+ C:/Users/sahar/repo/nucleof7_design_patterns/0_test/chip_headers/CMSIS/Include/cmsis_version.h \
+ C:/Users/sahar/repo/nucleof7_design_patterns/0_test/chip_headers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/sahar/repo/nucleof7_design_patterns/0_test/chip_headers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/sahar/repo/nucleof7_design_patterns/0_test/chip_headers/CMSIS/Include/mpu_armv7.h \
+ C:/Users/sahar/repo/nucleof7_design_patterns/0_test/chip_headers/CMSIS/Device/ST/STM32F7xx/Include/system_stm32f7xx.h
+C:/Users/sahar/repo/nucleof7_design_patterns/0_test/chip_headers/CMSIS/Device/ST/STM32F7xx/Include/stm32f7xx.h:
+C:/Users/sahar/repo/nucleof7_design_patterns/0_test/chip_headers/CMSIS/Device/ST/STM32F7xx/Include/stm32f765xx.h:
+C:/Users/sahar/repo/nucleof7_design_patterns/0_test/chip_headers/CMSIS/Include/core_cm7.h:
+C:/Users/sahar/repo/nucleof7_design_patterns/0_test/chip_headers/CMSIS/Include/cmsis_version.h:
+C:/Users/sahar/repo/nucleof7_design_patterns/0_test/chip_headers/CMSIS/Include/cmsis_compiler.h:
+C:/Users/sahar/repo/nucleof7_design_patterns/0_test/chip_headers/CMSIS/Include/cmsis_gcc.h:
+C:/Users/sahar/repo/nucleof7_design_patterns/0_test/chip_headers/CMSIS/Include/mpu_armv7.h:
+C:/Users/sahar/repo/nucleof7_design_patterns/0_test/chip_headers/CMSIS/Device/ST/STM32F7xx/Include/system_stm32f7xx.h:
